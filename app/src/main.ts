adab6154fdef9bf9e12ebfb2295c63833371898b
boonpat.papob <boonpat.papob@gmail.com>
2022-01-04 14:52:33 +0700

    add backend

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from "electron";
+import { exec } from "child_process";
+import { app, BrowserWindow, shell } from "electron";
 
 let mainWindow: BrowserWindow | null;
 const createWindow = () => {
@@ -6,7 +7,8 @@ const createWindow = () => {
     width: 1000,
     height: 500,
     webPreferences: {
-      preload: __dirname + "/preload.js",
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
   mainWindow.loadFile(__dirname + "/../index.html");
@@ -17,6 +19,12 @@ const createWindow = () => {
   });
 };
 
+app.on("will-finish-launching", () => {
+  shell.openPath(__dirname + "/../../backend/build/electronDeamon.app");
+});
+app.on("before-quit", () => {
+  exec("killall electronDeamon");
+});
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
--- app/src/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge } from "electron";
-import { cpus } from "os";
-
-export const API = {
-  threads: cpus().length,
-};
-contextBridge.exposeInMainWorld("api", API);
